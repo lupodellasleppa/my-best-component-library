aa9bd7c56adca8042922492afca1d1095ff7c593
Yoske Apolloni <lupodellasleppa@gmail.com>
2026-04-23 23:21:35 +0200

    feat: circular progress component

---
+++ src/components/MbcCircularProgress.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import '@material/web/progress/circular-progress.js'
+
+const props = defineProps<{
+  text?: string
+}>()
+</script>
+
+<template>
+  <div class="progress-container">
+    <md-circular-progress indeterminate />
+    {{ text || 'Loading...' }}
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.progress-container {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  flex-direction: column;
+  gap: var(--md-sys-shape-medium);
+}
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import "./assets/main.css";
+import './assets/main.css'
 
-export { default as MbcBadgeFilled } from "./components/MbcBadgeFilled.vue";
-export { default as MbcBadgeOutlined } from "./components/MbcBadgeOutlined.vue";
-export { default as MbcStepper } from "./components/MbcStepper.vue";
-export { default as MbcTooltip } from "./components/MbcTooltip.vue";
-export { default as MbcButtonDropdown } from "./components/MbcButtonDropdown.vue";
-export { default as MbcStickyHeader } from "./components/MbcStickyHeader.vue";
-export { default as MbcToaster } from "./components/MbcToaster.vue";
-export { default as MbcCopyChip } from "./components/MbcCopyChip.vue";
-export { useToast } from "./composables/useToast";
+export { default as MbcBadgeFilled } from './components/MbcBadgeFilled.vue'
+export { default as MbcBadgeOutlined } from './components/MbcBadgeOutlined.vue'
+export { default as MbcStepper } from './components/MbcStepper.vue'
+export { default as MbcTooltip } from './components/MbcTooltip.vue'
+export { default as MbcButtonDropdown } from './components/MbcButtonDropdown.vue'
+export { default as MbcStickyHeader } from './components/MbcStickyHeader.vue'
+export { default as MbcToaster } from './components/MbcToaster.vue'
+export { default as MbcCopyChip } from './components/MbcCopyChip.vue'
+export { default as MbcCircularProgress } from './components/MbcCircularProgress.vue'
+
+export { useToast } from './composables/useToast'
